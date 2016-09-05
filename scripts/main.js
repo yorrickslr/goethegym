67dc74d956ac0361fbe7c556f99620600a3fbf82
yorrickslr <yoyo.sieler@gmail.com>
2016-09-05 15:47:54 +0200

    Add tile scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,67 @@
-var debug = 1;
-
 var body;
 var main;
 var article;
+var footer;
+
+// mainpage
+var tiles;
+var tilepos = 0;
+
+function log(string) {
+	//console.log(string);
+}
 
 function initialize() {
-	if(debug) console.log("initialize()");
+	log("initialize()");
 	if(!document.addEventListener) {
 		alert("Diese Website unterstützt Ihre Browserversion nur eingeschränkt. " +
 			"Verwenden Sie bitte einen neueren Browser wie z.B. Firefox.");
 	} else {
 		body = document.getElementsByTagName("body")[0];
 		main = document.getElementsByTagName("main")[0];
-		setMain();
-		window.addEventListener("resize",setMain);
+		footer = document.getElementsByTagName("footer")[0];
 	}
 }
 
-function setMain() {
-	if(debug) console.log("setMain()");
+function initMain() {
+	initialize();
+	tiles = document.getElementsByClassName("tilewrapper")[0];
+	log("initMain()");
+	main.style.padding = "0";
+	main.style.top = "50px";
+	main.style.position = "absolute";
 	main.style.width = document.body.clientWidth + "px";
+	window.addEventListener("resize",function(){
+		main.style.width = document.body.clientWidth + "px";
+	});
+	main.style.height = tiles.offsetHeight + tiles.offsetTop + "px"; 
+	main.style.overflow = "hidden";
+	tiles.style.position = "absolute";
+	tiles.style.top = tiles.offsetTop + "px";
+	tiles.style.left = 0;
+	tiles.style.overflow = "hidden";
+	tiles.style.transitionDuration = ".5s";
+	footer.style.position = "absolute";
+	footer.style.top = main.offsetHeight + 50 + "px";
 }
 
-function setWrapper() {
+function scrollTiles(count) {
+	if(tilepos+count < 0) {
+		tilepos = 0;
+	} else if(tilepos+count+1 >= tiles.children.length) {
+		tilepos = tiles.children.length - 1;
+	} else {
+		tilepos += count;
+	}
+	tiles.style.left = -tilepos * 395 + "px";
+}
 
+function initSub() {
+	initialize();
+	log("initSub()");
+}
+
+function setMain() {
+	log("setMain()");
+	main.style.width = document.body.clientWidth + "px";
 }
